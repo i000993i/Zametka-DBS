c254beab5ba91bf3aff994ed566bf60698987554
i000993i <m62803141@gmail.com>
2026-07-07 22:24:45 +0300

    bump version to 0.2.0

--- zametka_dbs/core/version.py
+++ zametka_dbs/core/version.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 __author__ = "Zametka Team"
 __repo__ = "https://github.com/i000993i/Zametka-DBS"
 __api_url__ = "https://api.github.com/repos/i000993i/Zametka-DBS/releases/latest"
